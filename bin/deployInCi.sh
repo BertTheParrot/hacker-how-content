36ee8cb824108806163b738b3f9b6b290e79e7c0
Richard Gill <richard@rgill.co.uk>
2016-03-08 20:12:03 +0000

    Got repo ready for prod

--- bin/deployInCi.sh
+++ bin/deployInCi.sh
@@ -1,3 +1,6 @@
 if [ "$CIRCLECI" == "true" ] && [ "$CIRCLE_BRANCH" == "master" ] ; then
-    npm run start
+    NODE_ENV=development npm run start
+fi
+if [ "$CIRCLECI" == "true" ] && [ "$CIRCLE_BRANCH" == "production" ] ; then
+    NODE_ENV=production npm run start
 fi
--- scripts/config/production.js
+++ scripts/config/production.js
@@ -1,3 +1,4 @@
 export default {
-  firebaseUrl: 'https://hacker-how.firebaseio.com'
+  firebaseUrl: 'https://hacker-how.firebaseio.com',
+  firebaseSecret: 'NSb9CAykz4Y2DX0P1H1Bzpl0utslKzZaAAECeQ91'
 }
